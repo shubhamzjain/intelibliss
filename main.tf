b93df742a267d20df96d5096661b5911eb2c638d
shubhamzjain <91875964+shubhamzjain@users.noreply.github.com>
2022-02-14 20:46:21 +0530

    Update main.tf
    indentation change

--- main.tf
+++ main.tf
@@ -34,9 +34,13 @@ variable "instance_shape" {
   default = "VM.Standard.A1.Flex" # Or VM.Standard.E2.1.Micro
 }
 
-variable "instance_ocpus" { default = "1" }
+variable "instance_ocpus" { 
+  default = "1" 
+}
 
-variable "instance_shape_config_memory_in_gbs" { default = "6" }
+variable "instance_shape_config_memory_in_gbs" { 
+  default = "6" 
+}
 
 data "oci_identity_availability_domain" "ad" {
   compartment_id = var.tenancy_ocid
